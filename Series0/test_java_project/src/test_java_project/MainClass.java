37b54865ba707f10947041214b533b004cdb22bd
haren <lukasz.harezlak@gmail.com>
2013-11-05 16:12:15 +0100

    Repo setup

---
+++ Series0/test_java_project/src/test_java_project/MainClass.java
@@ -0,0 +1,15 @@
+package test_java_project;
+
+public class MainClass {
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+	}
+	
+	public static void method1(){
+		System.out.println("I am method 1!");
+	}
+	
+	public static void method2(String[] args){
+		System.out.println("I am method 2!");
+	}
+}
